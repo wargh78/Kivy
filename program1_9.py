c29516157cd13eaf21b7ecb45f8a9be7afa9dc66
wargh78 <164818267+wargh78@users.noreply.github.com>
2024-03-25 01:54:43 +0100

    Initial

---
+++ program1_9.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+class Interface(FloatLayout):
+    def display_information(self):
+        data = self.ids.textInput.text
+        self.ids.label.text = data
+
+class program1_9(App):
+    pass
+
+program1_9().run()
